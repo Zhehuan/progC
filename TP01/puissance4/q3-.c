aa994c8fd7ab4369f90704cbe8b7b7ce8ca34c79
Anas Yahyaoui <anas.yahyaoui@etu.u-pec.fr>
2017-09-18 16:17:30 +0200

    MAJ

--- TP01/puissance4/q3-.c
+++ TP01/puissance4/q3-.c
@@ -1,46 +1,65 @@
 #include <stdio.h>
 
-int main()
+//chercher la premiere ligne vide et placer le numero a cette position
+void **cherche(int size, int colonne, int **grille, int player)
 {
-	int grille[5][5];
-	int colonne = -1, ligne = 0;
+  int ligne = 0;
+  while (ligne < size)
+  {
+    if (grille[ligne][colonne] == 0)
+    {
+      grille[ligne][colonne] == player;
+      return **grille;
+    }
+    else
+      ligne++;
+  }
+  if (ligne == size)
+    printf("Please enter an other colon.");
+}
 
-	printf("Entrez un numero de colonne: \n");
-	scanf("%d", &colonne);
-	while (ligne < 5)
-	{
-		if (grille[ligne][colonne] == 0)
-			printf("Premiere ligne vide est la ligne %d.\n", ligne);
-		else
-			ligne++;
-	}
-	return 0;
+void verif()
+{
+  while (i < size)
+  {
+    j = 0;
+    while (j < size)
+    {
+      if (grille[i][j] == grille[i + 1][j + 1] && grille[i + 1][j + 1] ==
+grille[i + 2][j + 2] || grille)
+      {
+        win = 1;
+        printf("The player%d wins!", player);
+        j++;
+      }
+      else
+        j++;
+    }
+    i++;
+  }
 }
 
 int main()
 {
-	int grille[5][5] = {{3, 1, 1, 0, 1},
-		{2, 1, 5, 4, 2},
-		{1, 2, 4, 5, 3},
-		{3, 4, 2, 3, 5},
-		{0, 6, 3, 3, 7}};
-	int i = 0, j;
+  //creation de grille
+  int size = -1;
+  printf("The size of grill is : ");
+  scanf("%d", &size);
+  int grille[size][size];
 
-	while (i < 3)
-	{
-		j = 0;
-		while (j < 3)
-		{
-			if (grille[i][j] == grille[i + 1][j + 1] && grille[i + 1][j + 1] == grille[i + 2][j + 2])
-			{
-				printf("%dx%d\n", i + 1, j + 1);
-				j++;
-			}
-			else
-				j++;
-		}
-		i++;
-	}
-	return 0;
+  //saisir une colonne
+  int col = -1;
+  printf("Entrez un numero de colonne: \n");
+  while (col < 0 || col > size - 1)
+    scanf("%d", &col);
+
+//à compléter
+  while ()
+    cherche(size, colonne, grille, player)
+
+      int win =  0;
+  while (win = 0)
+  {
+  }
 }
 
